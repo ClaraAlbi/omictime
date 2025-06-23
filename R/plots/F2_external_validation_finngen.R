d7bd5f5695a1ce8f8fd9fe76dd588362884cc271
Clara Albinana <albinanaclara@gmail.com>
2025-06-23 14:36:10 +0000

    code replication

---
+++ R/plots/F2_external_validation_finngen.R
@@ -0,0 +1,9 @@
+
+
+lgb1 <- readRDS("data/cv.finngen_lightgbm_cv1.rds")
+xgb <- readRDS("data/cv.finngen_xgb_cv1.rds")
+lasso <- readRDS("data/cv.finngen_lasso_cv1.rds")
+lassox2 <- readRDS("data/cv.finngen_lassox2_cv1.rds")
+
+
+
