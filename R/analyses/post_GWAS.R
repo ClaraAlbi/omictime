8384f069d9ae66eb7df62b12e5c3d0edcec055ce
Clara Albiñana <claraalbinana@PSYMAC5.local>
2025-07-25 10:32:30 +0100

    code

--- R/analyses/post_GWAS.R
+++ R/analyses/post_GWAS.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-gwas <- data.table::fread("~/Downloads/gwas_absgap_rint.absgap_rint.glm.linear") %>%
+gwas <- data.table::fread("~/Downloads/gwas_res.res.glm.linear.gz") %>%
   filter(TEST == "ADD")
 
 time <-  data.table::fread("~/Downloads/gwas_time_day.time_day.glm.linear")
@@ -31,7 +31,7 @@ for (i in 1:nrow(sig)) {
   snp_id  <- sig$ID[i]
 
   if (!snp_id %in% result$ID) {
-    snp_chr <- sig$`#CHROM`[i]
+    snp_chr <- sig$`CHROM`[i]
     snp_pos <- sig$POS[i]
 
     genes_on_chr <- gene_seq[as.numeric(gene_seq$V1) == snp_chr, ]
@@ -69,13 +69,13 @@ sig <- gwas %>%
 
 # Compute cumulative BP position
 chr_info <- sig %>%
-  group_by(`#CHROM`) %>%
+  group_by(`CHROM`) %>%
   summarize(chr_len = max(POS)) %>%
-  arrange(`#CHROM`) %>%
+  arrange(`CHROM`) %>%
   mutate(chr_start = lag(cumsum(as.numeric(chr_len)), default = 0))
 
 sig <- sig %>%
-  left_join(chr_info, by = "#CHROM") %>%
+  left_join(chr_info, by = "CHROM") %>%
   mutate(BP_cum = as.numeric(POS) + chr_start)
 
 # Chromosome label positions
@@ -84,15 +84,21 @@ axis_df <- chr_info %>%
 
 # Plot
 res <- sig %>%
-  left_join(result, by = c("#CHROM" = "chr", "POS" = "pos"))
+  left_join(result, by = c("CHROM" = "chr", "POS" = "pos"))
 
 res %>%
   ggplot(aes(x = BP_cum, y = -log10(P))) +
   geom_point(size = 1.2, alpha = 0.75) +
-  geom_text(aes(label = gene_id),
-            vjust = -0.5, size = 2, check_overlap = TRUE) +
-  scale_x_continuous(breaks = axis_df$center, labels = axis_df$`#CHROM`) +
-  labs(x = "Chromosome", y = "-log10(p)", title = "Significant GWAS Hits by Phenotype") +
+  geom_text(
+    data            = subset(res, P < 1e-7),
+    aes(x = BP_cum, y = -log10(P), label = gene_id),
+    vjust           = -0.5,
+    hjust = -0.2,
+    size            = 2,
+    check_overlap   = TRUE
+  ) +
+  scale_x_continuous(breaks = axis_df$center, labels = axis_df$`CHROM`) +
+  labs(x = "Chromosome", y = "-log10(p)") +
   theme_bw() +
   theme(
     panel.grid.major.x = element_blank(),
