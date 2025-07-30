6731722aff11826ec54d887a76ff4f03dddaac71
Clara Albinana <albinanaclara@gmail.com>
2025-07-30 11:43:09 +0000

    new

--- R/analyses/aov_olink.R
+++ R/analyses/aov_olink.R
@@ -2,40 +2,29 @@ library(tidyr)
 library(dplyr)
 library(glue)
 library(stringr)
-
-install.packages("janitor")
 install.packages("broom")
-install.packages('bit64')
 
 type <- "olink"
 
 rint <- function(x) {
   ranks <- rank(x, ties.method = "average")
-  
+
   # Calculate the rank-inverse normal transformation
   n <- length(ranks)
   qnorm((ranks - 0.5) / n)
 }
 
-time <- readRDS("/mnt/project/biomarkers/time.rds")
+time <- readRDS("/mnt/project/clara/time.rds")
+covs <- readRDS("/mnt/project/clara/covs.rds")
+gen_covs <- readRDS("/mnt/project/clara/gencovs.rds")
 
-covs <- data.table::fread("/mnt/project/circadian/data/covariates.tsv") %>%
-  janitor::clean_names() %>%
-  mutate(across(c(2, 7), as.factor),
-         bmi = `x21002_0_0` / (`x50_0_0`/100)^2)
-colnames(covs) <- c("eid", "sex", "birth_year", "age_recruitment", "height", "assessment_centre", "month_attending", "weight", "smoking", "bmi")
-
-gen_covs <- data.table::fread("/mnt/project/genetic_covs.tsv") %>%
-  janitor::clean_names() %>%
-  select(eid, x22000_0_0, x22006_0_0, x22021_0_0, x22009_0_1:x22009_0_20)
-colnames(gen_covs) <- c("eid", "genotype_batch", "is_white", "rel", paste0("PC", 1:20))
-
-olink_times <- data.table::fread("/mnt/project/olink_processing_start_date.dat.txt")
-batch <- data.table::fread("/mnt/project/olink_batch_number.dat.txt")
-panel <- data.table::fread("/mnt/project/olink_assay.dat") %>%
+olink_times <- data.table::fread("/mnt/project/Bulk/Protein biomarkers/Olink/helper_files/olink_processing_start_date.dat")
+batch <- data.table::fread("/mnt/project/Bulk/Protein biomarkers/Olink/helper_files/olink_batch_number.dat")
+panel <- data.table::fread("/mnt/project/Bulk/Protein biomarkers/Olink/helper_files/olink_assay.dat") %>%
   mutate(prot = tolower(Assay), Panel = str_remove(Panel, " ")) %>% select(prot, Panel)
 
-metadata <- data.table::fread("/mnt/project/olink_instance_0_meta.csv") %>%
+#metadata <- data.table::fread("/mnt/project/clara/olink_i0_meta.csv") %>%
+metadata <- data.table::fread("olink_i0_meta.csv") %>%
   rename(num_prots = p30900_i0,
          PlateID = p30901_i0,
          well_id = p30902_i0,
@@ -50,27 +39,26 @@ metadata <- data.table::fread("/mnt/project/olink_instance_0_meta.csv") %>%
   filter(Batch != 0)
 
 
-prots <- data.table::fread("/mnt/project/olink_instance_0.csv") %>%
+prots <- data.table::fread("/mnt/project/clara/olink_i0.csv") %>%
   select(-glipr1)
 
 
 for (b in seq(2, ncol(prots), 100)){
-  #for (b in seq(1102, ncol(prots), 100)) {
   l1 <- list()
   l2 <- list()
   l3 <- list()
-  
+
   end_col <- min(b + 99, ncol(prots))
-  
+
   for (i in b:end_col) {
-    
-    print(i) 
+
+    print(i)
     var <- colnames(prots)[i]
     print(var)
     p <- panel$Panel[which(var == panel$prot)]
-    
+
     covs_c <- metadata %>% select(eid, Batch, ppp_sel, any_of(p))
-    
+
     d <- tibble(eid = prots$eid, raw = prots[[i]]) %>%
       drop_na() %>%
       left_join(gen_covs %>% select(eid, any_of(paste0("PC", 1:20)))) %>%
@@ -79,16 +67,14 @@ for (b in seq(2, ncol(prots), 100)){
       left_join(covs_c) %>%
       filter(time_day > 0) %>%
       filter(fasting < 24) %>%
-      mutate(smoking = case_when(smoking == -3 ~ NA_integer_,
-                                 TRUE ~ smoking),
-             across(c(assessment_centre, Batch, ppp_sel, sex, age_recruitment, smoking), as.factor),
+      mutate(across(c(assessment_centre, Batch, ppp_sel, sex, age_recruitment, smoking), as.factor),
              rint_b = rint(raw)) %>%
       select(eid,
              raw,
              rint_b,
              time_day,
-             sex, 
-             age_recruitment, 
+             sex,
+             age_recruitment,
              fasting,
              assessment_centre,
              month_attending,
@@ -101,43 +87,43 @@ for (b in seq(2, ncol(prots), 100)){
       filter(if_all(where(is.factor), ~ . %in% names(which(table(.) >= 50)))) %>%
       mutate(across(where(is.factor), droplevels))  %>%
       filter(time_day >= 9 & time_day <= 20)
-    
+
     mod <- aov(rint_b ~ . + sex * age_recruitment, data = d %>% select(-eid, -raw))
     out_aov <- broom::tidy(mod) %>% mutate(pr2 = sumsq / sum(sumsq),
                                            phen = var)
     l1 <- c(l1, list(out_aov))
-    
+
     m <- lm(rint_b ~ . + sex * age_recruitment, data = d %>% select(-eid, -raw, -time_day, -bmi, -smoking), na.action = "na.exclude")
     res_rint <- residuals(m)
-    
+
     m_time_only_cos <- lm(res_rint ~ cos(2 * pi * time_day / 24) + sin(2 * pi * time_day / 24), data = d)
-    
+
     out <- tibble(eid = d$eid, raw = d$raw, res = res_rint)
     l2 <- c(l2, list(out))
-    
+
     effects <- broom::tidy(m_time_only_cos , conf.int = TRUE) %>% mutate(phen = var)
     l3 <- c(l3, list(effects))
   }
-  
-  
+
+
   output_aov <- do.call(bind_rows, l1)
   saveRDS(output_aov, glue("aov_{type}_{b}.rds"))
-  
+
   output_effects <- do.call(bind_rows, l3)
   saveRDS(output_effects, glue("effects_{type}_{b}.rds"))
-  
+
   output_res <- tibble(res = l2) %>%
     mutate(phen = colnames(prots)[b:end_col]) %>%
     unnest(res) %>%
     pivot_wider(id_cols = eid, names_from = phen, values_from = res)
-  
+
   saveRDS(output_res, glue("res_{type}_{b}.rds"))
-  
+
   output_raw <- tibble(res = l2) %>%
     mutate(phen = colnames(prots)[b:end_col]) %>%
     unnest(res) %>%
     pivot_wider(id_cols = eid, names_from = phen, values_from = raw)
-  
+
   saveRDS(output_raw, glue("raw_{type}_{b}.rds"))
-  
+
 }
--- R/analyses/time_var.R
+++ R/analyses/time_var.R
@@ -6,28 +6,33 @@ library(lubridate)
 library(ggplot2)
 library(purrr)
 
-time <- data.table::fread("/mnt/project/blood_sampling.tsv") %>%
-  mutate(max_time = pmax(`3166-0.0`, `3166-0.1`, `3166-0.2`, `3166-0.3`, `3166-0.4`, `3166-0.5`, na.rm = T)) %>%
-  separate(max_time, into = c("date", "time"), sep = " ") %>%
+time <- data.table::fread("/mnt/project/clara/blood_sampling_i0.csv") %>%
+  mutate(max_time = pmax(p3166_i0_a0, p3166_i0_a1, p3166_i0_a2, p3166_i0_a3, p3166_i0_a4, p3166_i0_a5, na.rm = T)) %>%
+  separate(max_time, into = c("date", "time"), sep = "T") %>%
   separate(time, into = c("h", "min", "s"), sep = ":") %>%
   separate(date, into = c("y", "m", "d"), sep = "-", remove = F) %>%
   mutate(across(y:s, as.numeric),
          time_day = h + min/60) %>%
-  rename(fasting = `74-0.0`,
-         num_bsamples = `68-0.0`,
+  rename(fasting = p74_i0,
+         num_bsamples = p68_i0,
          date_bsampling = date)
 
-covs <- data.table::fread("/mnt/project/covariates.tsv") %>%
+covs <- data.table::fread("/mnt/project/clara/covariates.csv") %>%
   mutate(across(c(2,3, 6, 7, 9), as.factor),
-         bmi = `21002-0.0` / (`50-0.0`/100)^2,
-         smoking = case_when(`20116-0.0` == "-3" ~ NA_character_,
-                             TRUE ~ `20116-0.0`)) %>% select(-"50-0.0", -"21002-0.0", -"20116-0.0")
+         bmi = p21002_i0 / (p50_i0/100)^2,
+         smoking = case_when(p20116_i0 == "-3" ~ NA_character_,
+                             TRUE ~ p20116_i0)) %>% select(-p50_i0, -p21002_i0, -p20116_i0)
 
 colnames(covs) <- c("eid", "sex", "birth_year",  "age_recruitment",  "assessment_centre", "month_attending", "bmi", "smoking")
 
-gen_covs <- data.table::fread("/mnt/project/genetic_covs.tsv") %>%
-  select(eid, "22009-0.1":"22009-0.20", `22006-0.0`, `22021-0.0`, `22000-0.0`)
+gen_covs <- data.table::fread("/mnt/project/clara/genetic_covs.csv") %>%
+  select(eid, p22009_a1:p22009_a20, p22006, p22021, p22000)
 colnames(gen_covs) <- c("eid", paste0("PC", 1:20), "is_white", "rel", "batch")
+
+
+saveRDS(time, "time.rds")
+saveRDS(covs, "covs.rds")
+saveRDS(gen_covs, "gencovs.rds")
 
 
 l <- list.files("/mnt/project/biomarkers_3", full.names = T)
