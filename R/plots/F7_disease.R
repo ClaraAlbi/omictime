edcf0a1a65cc5a2945cd5406e79364fde9dd4fd2
Clara Albinana <albinanaclara@gmail.com>
2025-11-03 15:11:21 +0100

    sa

--- R/plots/F7_disease.R
+++ R/plots/F7_disease.R
@@ -1,5 +1,9 @@
 #remotes::install_github("lcpilling/ukbrapR")
 library(ukbrapR)
+library(tidyr)
+library(dplyr)
+install.packages("broom")
+library(purrr)
 
 ckd <- ukbrapR:::codes_df_ckd
 
@@ -7,6 +11,7 @@ ukbrapR:::codes_df_test
 
 bp <- tribble(~condition, ~vocab_id, ~code,
               "bp", "ICD10","F31.2",
+              "bp", "ICD9", "296",
               "bp","Read2", "E11..11",
               "bp","Read2","E114.00",
               "bp","Read2","E115.00",
@@ -25,10 +30,20 @@ df <- readRDS("/mnt/project/olink_int_replication.rds") %>%
   filter(!is.na(time_day)) %>%
   filter(i == 0) %>%
   rowwise() %>%
-  mutate(pred_mean = mean(c(pred_lgb, pred_xgboost, pred_lasso, pred_lassox2)))  %>%
+  mutate(pred_mean = mean(c(pred_lgb, pred_xgboost, pred_lasso, pred_lassox2))) %>%
   left_join(diagnosis_df)
 
 df$res <- residuals(lm(pred_mean ~ time_day, data = df))
 
+a <- df %>%
+  pivot_longer(contains("prev")) %>%
+  group_by(name) %>% nest() %>%
+  mutate(m = map(data, ~glm(value ~ abs(res), data = .x, family = binomial)),
+         p = map(m, broom::tidy)) %>%
+  unnest(p)
 
-summary(glm(bipolar_disorder_bin_prev ~ abs(res), data = df, family = binomial))
+summary(glm(episode_MDD_severe_with_psychosis_bin_prev ~ abs(res), data = df, family = binomial))
+
+
+
+
