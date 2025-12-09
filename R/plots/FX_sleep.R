2cf28da69c22d4b4cb7cca0204f47b63d0b6e654
Clara Albinana <albinanaclara@gmail.com>
2025-12-09 17:07:53 +0000

    sleep ex

--- R/plots/FX_sleep.R
+++ R/plots/FX_sleep.R
@@ -1,4 +1,21 @@
 library(ggplot2)
+library(broom)
+
+sleep <- data.table::fread("/mnt/project/chronotype2.tsv") %>%
+  select(eid,
+         h_sleep = `1160-0.0`,
+         chrono = `1180-0.0`,
+         ever_insomnia = `1200-0.0`,
+         wakeup = `1170-0.0`) %>%
+  mutate(chrono = case_when(
+    chrono == 1 ~ "Definitely morning",
+    chrono == 2 ~ "Rather morning",
+    chrono == -1~ "Neither",
+    chrono == 3 ~ "Rather evening",
+    chrono == 4 ~ "Definitely evening",
+    TRUE ~ NA_character_)) %>% rename(chronotype_assess = chrono) %>%
+  mutate(chronotype_assess = factor(chronotype_assess, levels = c("Definitely morning", "Rather morning", "Neither", "Rather evening", "Definitely evening")),
+         chronotype_assess = relevel(chronotype_assess, ref = "Definitely morning"))
 
 sleep_new <- data.table::fread("/mnt/project/sleep_online.csv") %>%
   mutate(across(where(is.character), ~ na_if(., ""))) %>%
@@ -73,6 +90,10 @@ sleep_new <- data.table::fread("/mnt/project/sleep_online.csv") %>%
 
   )
 
+sleep_new %>%
+  mutate(y = lubridate::year(p30489)) %>%
+  count(y)
+
 df <- readRDS("/mnt/project/olink_int_replication.rds") %>%
   filter(!is.na(time_day)) %>%
   filter(i == 0) %>%
@@ -87,9 +108,59 @@ df %>%
   ggplot(aes(x = res, y = rmeq_chronotype)) +
   geom_boxplot()
 
-summary(lm(res ~ rmeq_chronotype, data = df))
-summary(lm(res ~ p30429, data = df))
+broom::tidy(lm(res ~ rmeq_chronotype, data = df))
+broom::tidy(lm(res ~ chronotype_assess, data = df))
 
 
 #### CHECK IF PEOPLE THAT CHANGE GROUP HAD HIGHER DYSREGULATION
 
+#Fluvial plot
+install.packages("ggalluvial")
+library(ggalluvial)
+
+alluvial_data <- sleep %>% inner_join(sleep_new) %>%
+  filter(!is.na(chronotype_assess) & !is.na(rmeq_chronotype)) %>%
+  count(chronotype_assess, rmeq_chronotype)
+
+
+ggplot(alluvial_data,
+       aes(axis1 = chronotype_assess,
+           axis2 = rmeq_chronotype,
+           y = n)) +
+  geom_alluvium(aes(fill = chronotype_assess), width = 1/12) +
+  geom_stratum(width = 1/12, fill = "grey90", color = "black") +
+  geom_text(stat = "stratum", aes(label = after_stat(stratum)), size = 3) +
+  scale_x_discrete(limits = c("Assessment Centre",  "rMEQ-Based"),
+                   expand = c(.05, .05)) +
+  labs(
+    y = "Number of Participants"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+
+
+### rMEQ compared to blood based
+
+df %>%
+  filter(!is.na(rmeq_chronotype)) %>%
+  ggplot(aes(x = factor(round(time_day, 0)), y = res, fill = rmeq_chronotype)) +
+  geom_boxplot() +
+  theme_minimal()
+
+chronotype_avgs <- df %>%
+  left_join(sleep) %>%
+  group_by(chronotype_assess) %>%
+  summarise(avg_res = mean(res, na.rm = TRUE))
+
+df2 <- df %>%
+  left_join(chronotype_avgs, by = "chronotype_assess") %>%
+  mutate(diff_from_type = res - avg_res) %>%
+
+df %>%
+  ggplot(aes(x = res, y = rmeq_score)) +
+  geom_smooth()
+
+summary(lm(res ~ rmeq_chronotype, data = df))
+summary(lm(res ~ chronotype_assess, data = df))
+summary(lm(res ~ rmeq_score, data = df))
