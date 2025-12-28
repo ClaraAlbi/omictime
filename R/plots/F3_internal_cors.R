10f8e2cc69a4f863cad4817532c8c7557ebd8810
Clara Albinana <albinanaclara@gmail.com>
2025-12-28 17:53:19 +0000

    f

--- R/plots/F3_internal_cors.R
+++ R/plots/F3_internal_cors.R
@@ -283,7 +283,7 @@ p3 <- make_pair_plot(2, 3)
 
 final_plot <-  cowplot::plot_grid(p3, p1, p2, nrow = 1)
 
-full <- cowplot::plot_grid(part1, p_c, final_plot, nrow = 3, labels = c("", "C", "D"), label_size = 18, rel_heights = c(1, 0.7, 1))
+full <- cowplot::plot_grid(part1, final_plot, p_c, nrow = 3, labels = c("", "C", "D"), label_size = 18, rel_heights = c(1, 1, 0.7))
 
 
 ggsave("plots/F4_combined.png", full, width = 10, height = 10)
